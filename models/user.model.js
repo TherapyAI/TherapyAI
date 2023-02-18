3cc5c5152b4c2e29fd542b7aef4113f7d0b65e7a
javideas <infojavideas@gmail.com>
2023-02-18 15:55:26 +0200

    login with authentification and registration

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
+const ADMIN_USERS = (process.env.ADMIN_USERS || 'admin@example.org')
+  .split(',')
+  .map(email => email.trim())
+
+
 const schema = new mongoose.Schema(
   {
     name: { type: String, required: [true, "name is required"] },
@@ -18,12 +23,21 @@ const schema = new mongoose.Schema(
       required: [true, "password is required"],
       minLength: [8, "min length: 8"],
     },
+    role: {
+      type: String,
+      enum: ['admin', 'patient'],
+      default: 'patient'
+    }
   },
   { timestamps: true }
 );
 
 schema.pre("save", function (next) {
   const user = this;
+
+  if (ADMIN_USERS.includes(user.email)) {
+    user.role = 'admin';
+  }
 
   if (user.isModified("password")) {
     bcrypt
