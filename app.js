630599205f5f601e0ce0cfa02b96f90120e8e5cd
Erin <erica@Erin.local>
2023-02-18 14:38:56 +0100

    fix error login

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const createError = require("http-errors");
+const { session, loadSessionUser } = require("./config/session.config");
 
 const express = require("express");
 const app = express();
@@ -11,14 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "hbs");
 app.set("views", `${__dirname}/views`);
 
+app.use(session);
+app.use(loadSessionUser);
+
 const router = require("./config/routes.config");
 app.use(router);
 
-// app.use((error, req, res, next) => {
-//   error = !error.status ? createError(500, error) : error;
-//   console.error(error);
-//   res.status(error.status).render(`errors/${error.status}`, { error });
-// });
+app.use((error, req, res, next) => {
+  error = !error.status ? createError(500, error) : error;
+  console.error(error);
+  res.status(error.status).render(`errors/${error.status}`, { error });
+});
 
 const port = 3000;
 
