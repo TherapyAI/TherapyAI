e291d45e4f7e31556fbfde0ed0d72bdbc0788fbe
javideas <infojavideas@gmail.com>
2023-02-22 14:33:39 +0200

    first step to long term memory for therapist

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -8,52 +8,69 @@ const openai = new OpenAIApi(configuration);
 
 module.exports.sendChat = (req, res, next) => { // TODO: 1st phase without user. 2nd phase with 1 user. 3rd phase 2 users
   // Message.find().then -> return Message.create({ message })
-  //add query de sort, me devuelve un array, reduce para convertir a 1 string usando \n
-  //const history = all the messages from mongoDB;
-  const patientPrefix = 'Patient: '
-  // req.body.message = history + patientPrefix + req.body.message;
-  req.body.message = patientPrefix + req.body.message;
-  const { message } = req.body;
-  
-  // create new message document in MongoDB
-  Message.create({ message })
-    .then(() => {
-      // generate response from OpenAI API
-      return openai.createCompletion({
-        model: "text-davinci-003",
-        prompt:
-          `//Imagine a conversation between a therapist (called "TherapyAi") and a patient. I will provide the patient's dialogue and you only will provide the therapist dialogue. Don't autocomplete the patient's dialogue. Create only the dialogue for the therapist taking in count the patient's answer and the patient's info. If the patient shows any kind of harmful behaviour, please advise the patient to seek for professional real help. //
-        ` +
-          message +
-          '" \n\n',
-        max_tokens: 64,
-        temperature: 0.5,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: ["\n"],
-      });
-    })
-    .then((response) => {
-      const reply = response.data.choices[0].text;
-      // create new reply message document in MongoDB
-      return Message.create({ message: reply });
-    })
-    .then(() => {
-      // load all messages and render chat UI
-      return Message.find();
-    })
+  let history = "";
+
+  const userId = req.user.id;
+  Message.find({ user: userId })
+    .populate('user')
     .then((messages) => {
-      res.render("chat", { messages });
+      if( messages != undefined) {
+        history = messages.reduce((accumulator, message) => {
+          return accumulator + message.message + '\n';
+        }, '');
+        console.log(history);
+      } else {
+        console.log("History empty");
+      }
     })
-    .catch((error) => {
-      console.error("An error occurred", error);
-      res.status(500).send("An error occurred");
-    });
+    .then(() => {
+      const patientPrefix = 'Patient: '
+      req.body.message = '<li>' + history + patientPrefix + req.body.message  + '</li>';
+      const { message } = req.body;
+      
+      req.body.user = req.user.id;
+      // create new message document in MongoDB
+      Message.create({ message, user: req.user.id })
+        .then(() => {
+          // generate response from OpenAI API
+          return openai.createCompletion({
+            model: "text-davinci-003",
+            prompt:
+              `//Imagine a conversation between a therapist (called "TherapyAi") and a patient. I will provide the patient's dialogue and you only will provide the therapist dialogue. Don't autocomplete the patient's dialogue. Create only the dialogue for the therapist taking in count the patient's answer and the patient's info. If the patient shows any kind of harmful behaviour, please advise the patient to seek for professional real help. //
+            ` +
+              message +
+              '" \n\n',
+            max_tokens: 64,
+            temperature: 0.5,
+            top_p: 1.0,
+            frequency_penalty: 0.0,
+            presence_penalty: 0.0,
+            stop: ["\n"],
+          });
+        })
+        .then((response) => {
+          const reply = response.data.choices[0].text;
+          // create new reply message document in MongoDB
+          return Message.create({ message: reply, user: req.user.id  });
+        })
+        .then(() => {
+          // load all messages and render chat UI
+          return Message.find();
+        })
+        .then((messages) => {
+          res.render("chat", { messages });
+        })
+        .catch((error) => {
+          console.error("An error occurred", error);
+          res.status(500).send("An error occurred");
+        });
+    })
 };
 
 module.exports.loadChat = (req, res, next) => {
-  Message.find()
+  const userId = req.user.id;
+  Message.find({ user: userId })
+    .populate('user')
     .then((messages) => {
       res.render("chat", { messages });
     })
